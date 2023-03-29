167f768c428e6f1c900870919deda0fc69507975
DhruvSathavara <dhruvsathavara7228@gmail.com>
2023-03-29 14:25:17 +0530

    ceramic

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import { Provider } from '@self.id/react'
+import '@/styles/globals.css'
+
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+
+export default function App ({Component,pageProps}){
+  return(
+    <Provider client={{ceramic:'testnet-clay'}}>
+      <Component {...pageProps}/>
+    </Provider>
+  )
+}
